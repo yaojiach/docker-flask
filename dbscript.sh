118088d13ffdc68b0aa97a2a3a2778939ef78106
yaojiach <jiachen.yao@outlook.com>
2018-08-16 23:32:47 -0400

    Fix typo

--- dbscript.sh
+++ dbscript.sh
@@ -2,7 +2,7 @@
 psql << EOF
     create database jwt;
     create user dev with password '12345';
-    grant all privileges on jwt to dev;
+    grant all privileges on database jwt to dev;
 EOF
 
 psql postgresql://dev:12345@localhost:5432/jwt -a -f dbscript.sql
